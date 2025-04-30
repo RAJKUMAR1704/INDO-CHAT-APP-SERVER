446ac29329bd31692c1b2a462ef0d32f63df6da5
RAJKUMAR1704 <rajkumarqre143@gmail.com>
2025-04-30 08:21:40 +0530

    change server

--- constants/config.js
+++ constants/config.js
@@ -3,6 +3,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:4173",
     "https://indo-chat-app.vercel.app",
+    "indo-chat-44ikqjese-raj-dipak-chavans-projects.vercel.app",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
